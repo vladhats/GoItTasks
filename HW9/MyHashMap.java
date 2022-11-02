fd903dccf0ef6a9c393b3b4c92486a4d8cfc4e5e
vladhats <hatsvladyslav@gmail.com>
2022-11-02 18:49:10 +0200

    HW10 First done !

--- HW9/MyHashMap.java
+++ HW9/MyHashMap.java
@@ -2,6 +2,25 @@ package HW9;
 
 import java.util.Stack;
 
-public class MyHashMap {
-    Stack
+public class MyHashMap <T>{
+    class Entry<K, V> {
+        final K key;
+        V value;
+        Entry<K, V> next;
+
+        public Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        
+    }
+    static final int DEF_CAPACITY = 16;
+    transient T[] table;
+
+    /**
+     * The number of key-value mappings contained in this map.
+     */
+    transient int size;
 }
